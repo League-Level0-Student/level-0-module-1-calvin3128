1aaa54f3140d62cc8f9458cc2428b5ff0578822a
League Student <league@iMac-8.attlocal.net>
2018-07-10 17:38:20 -0700

    ok

---
+++ src/extra/Ages.java
@@ -0,0 +1,18 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class Ages {
+
+	
+	public static void main(String[] args) {
+		
+	
+	
+	for (int age = 2001; age <= 2018; age++) {
+		System.out.println("In " + age + ", I was " + (age - 2001) + " years old."); 
+	}
+		
+		
+	}
+}
---
+++ src/extra/Unbirthday.java
@@ -0,0 +1,21 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+
+	
+public static void main(String[] args) {
+	
+	String day = JOptionPane.showInputDialog("When is your birthday?");
+	
+	if(day.equals("7/10")) {
+		JOptionPane.showMessageDialog(null, "Happy Birthday!!");
+	}
+	else {
+		JOptionPane.showMessageDialog(null, "Happy Unbirthday!!");
+	}
+	
+	
+}
+}
